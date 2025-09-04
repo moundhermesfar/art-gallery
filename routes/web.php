4e898018a35b36e35c20c38ad7e4d6307e5fc18a
moundher mesfar <mesfarmounether@gmail.com>
2025-09-04 22:18:30 +0100

    some cleaning in the project structure

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,3 @@ use App\Http\Controllers\ImageController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/download-image', [ImageController::class, 'download'])->name('image.download');
-Route::post('/toggle-favorite', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
